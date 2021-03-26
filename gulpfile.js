6df1d35448704cd8ac8041304d16f68fd6716466
@gregor.eisenhorn <v.arakelyan@itech-group.ru>
2021-03-26 20:01:13 +0400

    Saving files before refreshing line endings

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  plumber = require("gulp-plumpber"),
+  concat = require("gulp-concat"),
+
+
+
+
+
+
+// exports.default = ;
+
